88acc551e3893659211c12bb98b53e40be1b1c01
Sang Nguyen <sangnn2012@gmail.com>
2020-10-21 01:32:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const APP_ID = '583cacab'
+  const APP_KEY = '68010ea183f127431f5f7c1695d538b1'
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form className="search-form">
+        <h1>Hello React</h1>
+        <button 
+          type="submit"
+          className="search-button"
+          >
+          Search
+        </button>
+      </form>
+      <h1
+        onClick={() => setCounter(counter + 1)}
         >
-          Learn React
-        </a>
-      </header>
+          {counter}
+      </h1>
     </div>
   );
 }
